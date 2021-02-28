4fde840fc4f470ade3f85e4176a1d64c6f765851
YoussefE95 <YoussefElzein@tuta.io>
2021-02-27 17:42:12 -0800

    updated comments

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 These are the files for my Pop_OS! setup. 
 
-install.sh: Installs vs-code, spotify, discord, tutanota, mozillavpn, ranger, tweaks, icons, and copies all setting files
+install.sh: installs vs-code, spotify, discord, tutanota, mozillavpn, ranger, tweaks, and copies all setting files
 
 Fonts are Iosevka (https://github.com/be5invis/iosevka)
 
-The terminal and vs-code themes are Base16 Tomorrow Eighties themes
+The terminal and vs-code themes are Base16 Eighties
 
-https://github.com/aaron-williamson/base16-gnome-terminal.git has every Base16 theme for gnome terminal
+https://github.com/aaron-williamson/base16-gnome-terminal.git has every Base16 theme for the gnome terminal
--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 # update system
 sudo apt update && sudo apt upgrade && sudo apt autoremove && flatpak update
 
-# add ppa for numix circle icons
+# add ppa for mozillavpn
 sudo add-apt-repository ppa:mozillacorp/mozillavpn
 
-# install icons and usual apps
+# install usual apps
 sudo apt install mozillavpn ranger neofetch htop cmatrix default-jdk gconf2 gnome-tweak-tool code
 flatpak install flathub com.tutanota.Tutanota com.discordapp.Discord com.spotify.Client
 
@@ -19,4 +19,3 @@ gsettings set org.gnome.Terminal.Legacy.Settings headerbar false
 git clone https://github.com/aaron-williamson/base16-gnome-terminal.git ~/.config/base16-gnome-terminal
 cd ~/.config/base16-gnome-terminal/color-scripts
 ./base16-eighties-256.sh
-./base16-tomorrow-256.sh
