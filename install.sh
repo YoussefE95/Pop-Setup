733a538a8881748cabb9fb499f0200cf01dd58ba
Youssef <40011589+YoussefE95@users.noreply.github.com>
2021-05-12 15:25:47 -0700

    Add gotop

--- install.sh
+++ install.sh
@@ -8,6 +8,11 @@ sudo add-apt-repository ppa:regolith-linux/release
 sudo apt install regolith-desktop-standard i3xrocks-battery i3xrocks-bluetooth i3xrocks-memory i3xrocks-temp i3xrocks-wifi ranger neofetch htop cmatrix default-jdk gconf2 gnome-tweak-tool code
 flatpak install flathub com.tutanota.Tutanota com.discordapp.Discord com.spotify.Client com.todoist.Todoist
 
+# install gotop
+git clone --depth 1 https://github.com/cjbassi/gotop /tmp/gotop
+/tmp/gotop/scripts/download.sh
+sudo mv gotop /usr/local/bin/
+
 # copy all dot files and regolith looks
 rsync -a .bashrc .config .icons .themes ~/
 sudo mv gruvbox gruvbox-light onedark onelight /etc/regolith/styles/
