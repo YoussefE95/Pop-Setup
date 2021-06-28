3d51ccf1ac3c27133e5d7491bbe92b486a4533ec
younix <younix@pop-os.localdomain>
2021-06-27 22:23:31 -0700

    Added beautiful discord

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 #Pop!_OS basic setup
 * Updates system
-* Installs regularly used applications
+* Installs regularly used applications, beautiful discord, and regolith
 * Copies personal bashrc and config files
-* Finally, installs Regolith and all themes
--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 sudo apt update && sudo apt upgrade && sudo apt autoremove && flatpak update
 
 # install usual apps
-sudo apt install ranger neofetch htop cmatrix default-jdk gconf2 gnome-tweak-tool code
+sudo apt install ranger neofetch htop cmatrix default-jdk gconf2 gnome-tweak-tool code python3-pip
 flatpak install flathub com.tutanota.Tutanota com.spotify.Client com.todoist.Todoist
 
 # install gotop
@@ -10,8 +10,11 @@ git clone --depth 1 https://github.com/cjbassi/gotop /tmp/gotop
 /tmp/gotop/scripts/download.sh
 sudo mv gotop /usr/local/bin/
 
+# install beautiful discord
+python3 -m pip install -U https://github.com/leovoel/BeautifulDiscord/archive/master.zip
+
 # copy config files
 rsync -a .bashrc .config ~/
 
 # install regolith and themes
-git clone http://github.com/YoussefE95/RegoSet ~/Downloads/RegoSet && cd ~/Downloads/RegoSet && ./install.sh
+git clone http://github.com/YoussefE95/RegoLook ~/Downloads/RegoLook && cd ~/Downloads/RegoLook && ./install.sh
